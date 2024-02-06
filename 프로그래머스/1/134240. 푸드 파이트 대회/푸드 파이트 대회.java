1308b1acf37fc906bf4256a125fb71d80bff81e9
정민지 <135775059+Soonrang@users.noreply.github.com>
2024-02-06 20:28:44 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.15 ms, Memory: 88 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+
+            for(int i=1; i<food.length; i++){
+                for(int j=0; j<food[i]/2; j++){
+                    sb.append(i);
+                }
+            }
+
+            String orgNumber = String.valueOf(sb.toString());
+            String otherNumber = String.valueOf(sb.reverse());
+
+            String answer = orgNumber + "0" + otherNumber;
+
+            return answer;
+    }
+}
